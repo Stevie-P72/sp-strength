28d8035483893ea406f5f24130d2726c78a2c6d2
Stevie-P72 <stevier72@outlook.com>
2020-07-21 20:25:51 +0000

    completed services model and began adding services page content

--- services/models.py
+++ services/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Training_Type(models.Model):
     name = models.CharField(max_length=254)
-    image_url = models.URLField(max_length=1024)
     description = models.TextField()
     image = models.ImageField()
 
--- services/views.py
+++ services/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Training_Type
 # Create your views here.
+
+
+def services(request):
+    types = Training_Type.objects.all()
+    context = {
+        'types': types,
+    }
+    return render(request, 'services/index.html', context)
