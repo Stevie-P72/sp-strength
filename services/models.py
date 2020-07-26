ac614de0351b379bceb89423aaab6c98faf41e3a
Stevie-P72 <stevier72@outlook.com>
2020-07-26 19:59:54 +0000

    styled services page

--- services/models.py
+++ services/models.py
@@ -5,6 +5,7 @@ class Training_Type(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     image = models.ImageField()
+    price = models.DecimalField(decimal_places=2, max_digits=5, null=True, blank=True)
 
     def __str__(self):
         return self.name
