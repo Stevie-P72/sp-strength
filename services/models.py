902433732a0cb4c6407586018291ce8e3f1d231d
Stevie-P72 <stevier72@outlook.com>
2020-07-28 16:48:35 +0000

    added article to service models

--- services/models.py
+++ services/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Training_Type(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
+    article = models.TextField(null=True, blank=True)
     image = models.ImageField()
     price = models.DecimalField(decimal_places=2, max_digits=5, null=True, blank=True)
 
