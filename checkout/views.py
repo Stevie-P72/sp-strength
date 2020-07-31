da377169b612c15a43dd6e738e4104f7dee8acb6
Stevie-P72 <stevier72@outlook.com>
2020-07-31 01:51:15 +0000

    added checkouts form

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from services.models import Training_Type
+from .forms import PurchaseOrderForm
 # Create your views here.
+
+
+def checkout(request, article_name):
+    article_name = get_object_or_404(Training_Type, name=article_name)
+    purchase_order_form = PurchaseOrderForm()
+    context = {
+       'purchase_order_form': purchase_order_form,
+       'article_name': article_name,
+    }
+    return render(request, "checkout/index.html", context)
