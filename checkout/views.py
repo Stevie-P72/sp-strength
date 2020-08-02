4cdfe51c09bddfbc13935ded02c5bd16c9789d8c
Stevie-P72 <stevier72@outlook.com>
2020-08-02 18:09:28 +0000

    began setup of stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from services.models import Training_Type
 from .forms import PurchaseOrderForm
+from django.conf import settings
+
+import stripe
 # Create your views here.
 
 
 def checkout(request, article_name):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_PUBLIC_KEY
+
     article_name = get_object_or_404(Training_Type, name=article_name)
+    purchase_amount = round(article_name.price * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=purchase_amount,
+        currenct=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
+
     purchase_order_form = PurchaseOrderForm()
     context = {
        'purchase_order_form': purchase_order_form,
