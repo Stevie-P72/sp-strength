605c47df36df5abd1a79bf7f959049ef98caab21
Stevie-P72 <stevier72@outlook.com>
2020-08-02 14:27:56 +0000

    added stripe payment element

--- checkout/views.py
+++ checkout/views.py
@@ -10,5 +10,7 @@ def checkout(request, article_name):
     context = {
        'purchase_order_form': purchase_order_form,
        'article_name': article_name,
+       'client_secret': 'test',
+       'stripe_public_key': 'pk_test_51HBh7BBZlYFkG3UBGr91WrMsmK9On6r6U3XUrzeJErc9UqButB5248Y3MLppdPdDkDXolV3tAnRVG0TtUEQTIpIu00sm7Rid2L'
     }
     return render(request, "checkout/index.html", context)
