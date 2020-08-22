f18238b6d647d6e87a5aac984ce9d8d8e7b789eb
Stevie-P72 <stevier72@outlook.com>
2020-08-22 17:55:44 +0000

    small edits to various models

--- services/models.py
+++ services/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Training_Type(models.Model):
-    name = models.CharField(max_length=254)
+    name = models.CharField(max_length=64)
     description = models.TextField()
     article = models.TextField(null=True, blank=True)
     image = models.ImageField()
-    price = models.DecimalField(decimal_places=2, max_digits=5, null=True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=5, null=False, blank=False, default=9.99)
 
     def __str__(self):
         return self.name
