d6d7961fb92a9fb17470137043fcb7c336001afe
Stevie-P72 <stevier72@outlook.com>
2020-08-25 17:42:46 +0000

    corrected checkouts bug and added email port info

--- sp_strength/settings.py
+++ sp_strength/settings.py
@@ -163,3 +163,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+
